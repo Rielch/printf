2d1b9ca01aa77b3f53e4e4f3ee3c4c7dbfe46e18
Lunerio <lunerio@outlook.es>
2020-10-25 22:56:51 +0000

    repair conditions printing_func

--- printing_func.c
+++ printing_func.c
@@ -9,7 +9,6 @@ int print_c(va_list args)
 {
 	char c = va_arg(args, int);
 
-
 	write(1, &c, 1);
 	return (1);
 }
@@ -22,7 +21,7 @@ int print_c(va_list args)
  */
 int print_s(va_list args)
 {
-	int i;
+	int i = 0;
 	char *str;
 
 	str = va_arg(args, char *);
@@ -30,7 +29,7 @@ int print_s(va_list args)
 	{
 		str = "";
 	}
-	for (i = 0; str[i] != '\0'; i++)
+	for (; str[i] != '\0'; i++)
 	{
 		write(1, &str[i], 1);
 	}
